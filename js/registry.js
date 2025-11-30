55fab7d5d89b0eff8daf3536a0945674deac5417
Crypt212 <ahmed.elesseily.d@gmail.com>
2025-11-30 17:31:53 +0200

    add playfair alogrithm and add utils folder

--- js/registry.js
+++ js/registry.js
@@ -2,8 +2,9 @@ import  ceaser  from "./Chipers/ceaser.js";
 import autokey from "./Chipers/autokey.js";
 import hillcliming from "./Chipers/hillcliming.js";
 import affin from "./Chipers/affin.js";
+import playfair from "./Chipers/playfair.js";
 
-export const CHIPHERS = [ceaser, autokey,hillcliming, affin];
+export const CHIPHERS = [ceaser, autokey, hillcliming, affin, playfair];
 
 export function getCipherById(id) {
     return CHIPHERS.find(cipher => cipher.id === id);
